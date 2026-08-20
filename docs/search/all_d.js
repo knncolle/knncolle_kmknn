bc0ce42a6c81384cdfb9f21ca0cbdfb7a4458d63
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-08-20 06:28:54 +0000

    Deploying to gh-pages from @ knncolle/knncolle_kmknn@901152b14f7161e980306717990ebd4ce821494a 🚀

--- docs/search/all_d.js
+++ docs/search/all_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parallelize_0',['parallelize',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a5faf80c12bbfbab853c41f6d001fe073',1,'knncolle::parallelize()'],['https://ltla.github.io/CppKmeans/namespacekmeans.html#a554562a5b1c75d7494de4e541a77ca30',1,'kmeans::parallelize()']]],
+  ['parallelize_0',['parallelize',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a5faf80c12bbfbab853c41f6d001fe073',1,'knncolle::parallelize()'],['https://ltla.github.io/CppKmeans/namespacekmeans.html#a0e5998de24c447715a844a060ceab221',1,'kmeans::parallelize()']]],
   ['parallelize_2ehpp_1',['parallelize.hpp',['https://ltla.github.io/CppKmeans/parallelize_8hpp.html',1,'']]],
   ['power_2',['power',['../structknncolle__kmknn_1_1KmknnOptions.html#abc25b2afd6e77663dd7e971c1a4649d4',1,'knncolle_kmknn::KmknnOptions']]],
   ['prebuilt_3',['Prebuilt',['https://knncolle.github.io/knncolle/classknncolle_1_1Prebuilt.html',1,'knncolle']]],
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['parallelize_0',['parallelize',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a5faf80c12bbfbab853c41f6d001fe073',1,'knncolle::parallelize()'],['https://ltla.github.io/CppKmeans/namespacekmeans.html#a554562a5b1c75d7494de4e541a77ca30',1,'kmeans::parallelize()']]]
+  ['parallelize_0',['parallelize',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a5faf80c12bbfbab853c41f6d001fe073',1,'knncolle::parallelize()'],['https://ltla.github.io/CppKmeans/namespacekmeans.html#a0e5998de24c447715a844a060ceab221',1,'kmeans::parallelize()']]]
 ];
